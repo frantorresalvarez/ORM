7d4744feb8e50dcaed2b70eee69751bbe074c44c
frantorresalvarez <franciscotorresdj@gmail.com>
2024-02-05 16:53:24 +0100

    7

--- orm.py
+++ orm.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import random
+import math
+
 
 personas = []
 numeropersonas = 20
@@ -12,7 +14,7 @@ class Persona:
         self.posx = random.randint(0,1024)
         self.posy = random.randint(0,1024)
         self.radio = 30
-        self.direccion = 0
+        self.direccion = random.randint(0,360)
         self.color = "blue"
         self.entidad = ""
     def dibuja(self):
@@ -22,20 +24,40 @@ class Persona:
                            self.posy+self.radio/2,
                            fill=self.color)
     def mueve(self):
-        lienzo.move(self.entidad,500,0)
+        self.colisiona()
+        lienzo.move(self.entidad,
+                    math.cos(self.direccion),
+                    math.sin(self.direccion))
+        self.posx += math.cos(self.direccion)
+        self.posy += math.sin(self.direccion)
+    def colisiona(self):
+        if self.posx < 0 or self.posx > 1024 or self.posy < 0 or self.posy > 1024:
+            self.direccion += math.pi
 
 
-
+# CREO UNA VENTANA
 raiz = tk.Tk()
 
+#EN LA VENTANA CREO UN LIENZO
 lienzo = tk.Canvas(width=1024,height=1024)
 lienzo.pack()
 
+#EN LA COLECCION INTRODUZCO INSTANCIAS DE PERSONAS
 for i in range(0,numeropersonas):
     personas.append(Persona())
-
+    
+#PARA CADA UNA DE LAS PERSONAS DE LA COLECCION LAS PINTA
 for persona in personas:
     persona.dibuja()
-    persona.mueve()
+    
+#CREO UN BUCLE REPETITIVO    
+def bucle():
+    #PARA CADA PERSONA EN LA COLECCION
+    for persona in personas:    
+        persona.mueve()
+    raiz.after(10,bucle)
 
+    
+#EJECUTO EL BUCLE
+bucle()
 raiz.mainloop()
