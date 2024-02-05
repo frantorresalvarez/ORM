c4bd42024df1212274a48cdf0ed712202d7d3ffa
frantorresalvarez <franciscotorresdj@gmail.com>
2024-02-05 16:36:46 +0100

    3

--- orm.py
+++ orm.py
@@ -1,15 +1,27 @@
 import tkinter as tk
+import random
+
 
 class Persona:
     def __init__(self):
-        self.posx = 512
-        self.posy = 512
+        self.posx = random.randint(0,512)
+        self.posy = random.randint(0,512)
+        self.radio = 30
+    def dibuja(self):
+        lienzo.create_oval(self.posx-self.radio/2,
+                           self.posy-self.radio/2,
+                           self.posx+self.radio/2,
+                           self.posy+self.radio/2,
+                           fill="red")
 
 
 raiz = tk.Tk()
 
-lienzo = tk.Canvas(widh=1024,height=1024)
+lienzo = tk.Canvas(width=1024,height=1024)
 lienzo.pack()
 
+persona = Persona()
+persona.dibuja()
 
-rainz.mainloop()
+
+raiz.mainloop()
